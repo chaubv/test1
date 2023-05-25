328a00f86d1b3148333e2e1c684eadadb97127a3
chaubv <minhchau.utc@gmail.com>
2023-05-25 14:39:59 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 tag=`git rev-parse -short=10 HEAD | tail -n +2`
 echo $tag
-echo $params.appEnv 
-sed -i s/dev/${tag}/g /home/ubuntu/jenkins/multi-branch/test.yaml
+DB_NAME="postgres"
+date=$(date '+%Y:%m:%d-%H:%M')
+environment="env_temp"
+env_srv="env_service"
+# Set the username and password for the database
+DB_USERNAME="postgres"
+DB_PASSWORD="imip@2022"
+DB_HOST="192.168.2.208"
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p 5432 -U postgres -d $DB_NAME -c "INSERT INTO ai_metrics  (env, idcommit, nameservice, date) VALUES ('$environment', '$tag', '$env_srv', '$date');"
