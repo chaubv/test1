5e1ac7a9d5b2d06aaa56888cd8064d27c50d4cb3
chaubv <minhchau.utc@gmail.com>
2023-05-25 16:15:03 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,6 +7,6 @@ environment="env_temp"
 env_srv="env_service"
 # Set the username and password for the database
 DB_USERNAME="postgres"
-#DB_PASSWORD="imip@2022"
+#DB_PASSWORD="imip@202212"
 DB_HOST="192.168.2.208"
 PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p 5432 -U postgres -d $DB_NAME -c "INSERT INTO ai_metrics  (env, idcommit, nameservice, date) VALUES ('$environment', '$tag', '$env_srv', '$date');"
